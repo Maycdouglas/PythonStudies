31ac9d443346120dd133a723a2c2a1669f28223c
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2023-11-20 18:16:50 -0300

    Add informacoes sobre funcoes

---
+++ funcoes.py
@@ -0,0 +1,43 @@
+# FUNCÕES
+
+def funcao(nome, sobrenome = "Douglas"):
+    print(f'Meu nome é {nome} e meu sobrenome é {sobrenome}')
+
+funcao('Maycon')
+
+# Função com *args
+# permite que insira diversos argumentos em uma tupla
+# por convenção, utiliza-se args
+
+def soma(*args):
+    soma = 0
+    for x in args:
+        soma = soma + x
+    return soma
+
+print(soma(1,2,2,20))
+print(soma(1,2,3,4,5,6,7,8,9))
+
+# Exemplo de função recebendo função como argumento e também utilizando o *args (Empacotamento e Desempacotamento)
+def saudacao(msg, nome):
+    return f'{msg}, {nome}'
+
+def executa(funcao, *args):
+    return funcao(*args)
+
+print(
+    executa(saudacao,'Bom dia', 'Maycon')
+)
+
+# Exemplo de Closure
+
+def criar_saudacao(saudacao):
+    def saudar(nome):
+            return f'{saudacao}, {nome}!'
+    return saudar
+
+falar_bom_dia = criar_saudacao('Bom dia')
+falar_boa_noite = criar_saudacao('Boa noite')
+
+print(falar_bom_dia('Maycon'))
+print(falar_boa_noite('Maycon'))
