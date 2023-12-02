adaba565eb46b19de84556708b0b7025831ce634
Maycdouglas <mayconcruzeiroraposa@gmail.com>
2023-12-02 07:01:06 -0300

    Add try, except, else e finally

--- tryexcept.py
+++ tryexcept.py
@@ -10,3 +10,31 @@ except:
     print('Isso não é um número')
 
 # a forma acima não é a mais indicada
+
+#Captura de Erros
+try:
+    a = 18
+    b = 0
+    print(a[1000])
+    c = a/b
+    
+except ZeroDivisionError:
+    print('Dividiu por zero.')
+except NameError:
+    print('Nome n não está definido')
+except (TypeError, IndexError) as error:
+    print('TypeError + IndexError')
+    print('Error:', error)
+    print('Nome da classe:',error.__class__.__name__)
+except Exception:
+    print('Erro desconhecido')
+
+# Finally e Else
+try:
+    print('Abrir Arquivo')
+except:
+    print('Dividiu Zero')
+else: # executa somente se não tiver capturado erro
+    print('Não deu erro')
+finally: #finally é sempre executado
+    print('Fechar Arquivo')
